792b03b40cc755422bead4ffdb78abff1e5dde8d
Seb Lee-Delisle <seb@leedelisle.com>
2016-03-17 23:41:22 +0000

    added neopixel Color lib

--- 4.1-neopixel-rainbow.js
+++ 4.1-neopixel-rainbow.js
@@ -1,39 +1,34 @@
 var ws281x = require('rpi-ws281x-native');
+var Colour = require('color'); 
+
 
 var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
-    pixelData = new Uint32Array(NUM_LEDS);
+	pixelData = new Uint32Array(NUM_LEDS);
 
 ws281x.init(NUM_LEDS);
 
 // ---- trap the SIGINT and reset before exit
 process.on('SIGINT', function () {
-  ws281x.reset();
-  process.nextTick(function () { process.exit(0); });
+	ws281x.reset();
+	process.nextTick(function () { process.exit(0); });
 });
 
 
 // ---- animation-loop
 var offset = 0;
 setInterval(function () {
-  for (var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = colorwheel((offset + i) % 256);
-  }
-
-  offset = (offset + 1) % 256;
-  ws281x.render(pixelData);
+	for (var i = 0; i < NUM_LEDS; i++) {
+		var colour = Colour().hsl((offset + i), 100, 50).rgb(); 
+		pixelData[i] = rgb2Int(colour)
+		if(i==0) console.log(colour); 
+	}
+	offset++;
+	ws281x.render(pixelData);
 }, 1000 / 30);
 
 console.log('Press <ctrl>+C to exit.');
 
-
-// rainbow-colors, taken from http://goo.gl/Cs3H0v
-function colorwheel(pos) {
-  pos = 255 - pos;
-  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
-}
-
-function rgb2Int(r, g, b) {
-  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+function rgb2Int(col) {
+	var r = col[0], g=col[1], b=col[2]; 
+	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
