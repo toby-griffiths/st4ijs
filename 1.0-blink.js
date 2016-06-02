c032db2e171ff81cd7508f1516ab89d173cd7593
Seb Lee-Delisle <sebleedelisle@gmail.com>
2016-06-02 15:27:39 +0000

    updates

--- 1.0-blink.js
+++ 1.0-blink.js
@@ -2,7 +2,7 @@ var Gpio = require('onoff').Gpio;
 
 var led = new Gpio(21,'out'); 
 
-setInterval(update, 1000);
+var interval = setInterval(update, 1000);
 
 function update() { 
 	led.writeSync(1-led.readSync()); 
@@ -10,6 +10,7 @@ function update() {
 
 
 function exit() { 
+	clearInterval(interval);
 	led.unexport(); 
 }
 
--- 1.1-button.js
+++ 1.1-button.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio,
   led = new Gpio(21, 'out'),
-  button = new Gpio(23, 'in', 'both');
+  button = new Gpio(23, 'in');
 
 setInterval(update, 1); 
 
